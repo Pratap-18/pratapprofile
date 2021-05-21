628642731a7aea0948001ed3bd0009ef6a031fdc
pratap risal <prattap18@gmail.com>
2021-05-21 09:54:09 +0545

    ngo link

--- src/Components/Navbar/Nav.js
+++ src/Components/Navbar/Nav.js
@@ -3,19 +3,30 @@ import { Link } from 'react-router-dom'
 import './Nav.css'
 
 
+
+
 function Nav() {
     return (
         <div>
-            <nav>
-                <ul>
-                    <li> <Link to="/">Home</Link></li>
-                    <li> <Link to="/projects">Projects</Link></li>
-                    <li> <Link to="/certificates">Certificates</Link></li>
-                    <li> <Link to="/cv">CV</Link></li>
-                    <li> <Link to="/contact">Contact</Link></li>
-                </ul>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item"> <Link to="/" className="nav-link">Home</Link></li>
+                            <li className="nav-item"> <Link to="/projects" className="nav-link">Projects</Link></li>
+                            <li className="nav-item"> <Link to="/certificates" className="nav-link">Certificates</Link></li>
+                            <li className="nav-item"> <Link to="/cv" className="nav-link">CV</Link></li>
+                            <li className="nav-item"> <Link to="/contact" className="nav-link">Contact</Link></li>
+
+                        </ul>
+                    </div>
+                </div>
             </nav>
+
+
         </div>
+
     )
 }
 
